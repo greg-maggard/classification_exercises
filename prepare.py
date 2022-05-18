2dfa78bcb54a3a5d3eccedbfe7bfeef721da5da2
Greg Maggard <gregamaggard@gmail.com>
2022-05-18 11:31:30 -0500

    Added scripts to prepare.py that will clean telco data.

--- prepare.py
+++ prepare.py
@@ -56,8 +56,32 @@ def clean_iris_data(df):
     df = pd.concat([df, dummy_df], axis = 1)
     return df
 
-def prep_iris(df):
+def prep_iris_data(df):
     df = clean_iris_data(df)
     train, test = train_test_split(df, train_size = 0.8, stratify = df.species, random_state = 1234)
     train, validate = train_test_split(train, train_size = 0.8, stratify = train.species, random_state = 1234)
     return train, validate, test
+
+    #--------------------------------------------------------------------------------------------------------------------------------------------
+
+#Prepare Telco Data:
+def clean_telco_data(df):
+    #Replacing empty cells with nulls:
+    df = df.replace(' ', np.nan)
+    #Dropping rows with nulls:
+    df = df.dropna()
+    #Dropping unneeded columns:
+    columns_to_drop = ['contract_type_id', 'payment_type_id', 'internet_service_type_id',]
+    df = df.drop(columns = columns_to_drop)
+    #creating dummy variables of species
+    dummy_df = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'contract_type', 'payment_type', 'internet_service_type']], drop_first = True)
+    #concatenating dummy variables onto original dataframe
+    df = pd.concat([df, dummy_df], axis = 1)
+    return df
+
+def prep_telco_data(df):
+    df = clean_telco_data(df)
+    train, test = train_test_split(df, train_size = 0.8, stratify = df.churn, random_state = 1234)
+    train, validate = train_test_split(train, train_size = 0.8, stratify = train.churn, random_state = 1234)
+    return train, validate, test
+
