23d34e153f5e5d68f8b6c352f7483f0c833c8ca1
Greg Maggard <gregamaggard@gmail.com>
2022-05-23 16:29:49 -0500

    Made significant porgress on the telco project.

--- telco_classification_project/prepare.py
+++ telco_classification_project/prepare.py
@@ -75,7 +75,7 @@ def clean_telco_data(df):
     #Dropping rows with nulls:
     df = df.dropna()
     #Dropping unneeded columns:
-    columns_to_drop = ['contract_type_id', 'payment_type_id', 'internet_service_type_id',]
+    columns_to_drop = ['contract_type_id', 'payment_type_id', 'internet_service_type_id']
     df = df.drop(columns = columns_to_drop)
     #creating dummy variables of categorical columns
     dummy_df = pd.get_dummies(df[['gender', 'senior_citizen', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'contract_type', 'payment_type', 'internet_service_type']], drop_first = True)
